eaa4f35eafe7fe7b46c8c3d459bbf3980325f50b
(no author) <(no author)@f191e5f7-4e04-0410-925a-985e7dbff663>
2003-12-21 02:36:16 +0000

    This commit was manufactured by cvs2svn to create tag 'r0_40-2sls'.
    git-svn-id: svn+ssh://thor/srv/subversion/annvix/repos/packages@6800 f191e5f7-4e04-0410-925a-985e7dbff663

---
+++ removed/tags/r0_40-2sls/unpackages/msec/msec.sh
@@ -0,0 +1,39 @@
+#---------------------------------------------------------------
+# Project         : Mandrake Linux
+# Module          : msec
+# File            : msec.sh
+# Version         : $Id: msec.sh,v 1.11 2002/03/04 19:19:14 flepied Exp $
+# Author          : Yoann Vandoorselaere
+# Created On      : Wed Feb 13 18:35:58 2002
+# Purpose         : settings according to security level
+#---------------------------------------------------------------
+
+if [ -r /etc/sysconfig/msec ]; then
+	. /etc/sysconfig/msec
+fi
+
+if [ `id -u` -ge 500 ]; then
+    if [ -n "$UMASK_USER" ]; then
+	umask $UMASK_USER
+    else
+	umask 022
+    fi
+else
+    if [ -n "$UMASK_ROOT" ]; then
+	umask $UMASK_ROOT
+    else
+	umask 002
+    fi
+fi
+
+if [ -n "$SECURE_LEVEL" ]; then
+    if [ "$SECURE_LEVEL" -le 1 ] && ! echo ${PATH} | fgrep -q :.; then
+	export PATH=$PATH:.
+    fi
+fi
+
+export SECURE_LEVEL
+
+[ -n "$TMOUT" ] && typeset -r TMOUT
+
+# msec.sh ends here
