232d56b7741e8c8755e5b626cd5444f158ae2cdd
vdanen <vdanen@f191e5f7-4e04-0410-925a-985e7dbff663>
2005-02-27 07:09:33 +0000

    - 2.0.0-RC4 - apache 2.0.53 - fix license - rule out a number of perl requires (ala Mandrake and Fedora) - nuke fake Apache::Status as this is included and now works - don't include bundled Apache-Test - make "make test" work using stuff from Fedora and SUSE (oden)   - this only kinda works here; craps out on a SetEnv directive; not sure     why so disable for now - remove ADVX stuff
    git-svn-id: svn+ssh://thor/srv/subversion/annvix/repos/packages@2693 f191e5f7-4e04-0410-925a-985e7dbff663

--- branches/CURRENT_1_0/packages/httpd-mod_perl/apache2-mod_perl-startup.pl
+++ branches/CURRENT_1_0/packages/httpd-mod_perl/apache2-mod_perl-startup.pl
@@ -3,15 +3,10 @@ use Apache2 ();
 use lib qw(/var/www/perl);
 
 # If the mod_perl-common package is present, enable 
-# mod_perl 1.0 compatibility mode. Otherwise, we add
-# /var/www/perl/.modperl2 to the list of searched directories
-# for libraries, so we can, for example, put a dummy Apache::Status
-# since it's not present in mod_perl 2.0.
+# mod_perl 1.0 compatibility mode.
 use Config;
 if ( -e "$Config{'vendorarch'}/Apache.pm" ) {
    require Apache::compat
-} else {
-   import lib qw(/var/www/perl/.modperl2); 
 }
 
 use ModPerl::Util (); #for CORE::GLOBAL::exit
@@ -20,7 +15,7 @@ use Apache::RequestRec ();
 use Apache::RequestIO ();
 use Apache::RequestUtil ();
 
-use Apache::Server ();
+use Apache::ServerRec ();
 use Apache::ServerUtil ();
 use Apache::Connection ();
 use Apache::Log ();
